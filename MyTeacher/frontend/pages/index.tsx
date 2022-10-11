affc4911f14d861a19ffbc340f0fdea0a2bf0172
Luis Tomasini <100858288+luistomasini@users.noreply.github.com>
2022-10-10 22:36:30 -0300

    criacao de backend e integracao com frontend

--- MyTeacher/frontend/pages/index.tsx
+++ MyTeacher/frontend/pages/index.tsx
@@ -1,46 +1,57 @@
-import { Box } from '@mui/material'
+import { Box, Button, Dialog, DialogActions, Grid, TextField } from '@mui/material'
 import type { NextPage } from 'next'
 import { Professor } from '../src/@types/professor'
 import Lista from '../src/components/Lista/Lista'
+import { useIndex } from '../src/hooks/pages/useIndex'
 
 
 const Home: NextPage = () => {
-  
-  const professores: Professor[] = [
-    {
-      id: 1,
-      nome: "Professor 1",
-      foto: "https://github.com/luistomasini.png",
-      descricao: "Descricao do professor 1",
-      valor_hora: 100
-    },
-    {
-      id: 2,
-      nome: "Professor 2",
-      foto: "https://github.com/luistomasini.png",
-      descricao: "Descricao do professor 2",
-      valor_hora: 200
-    },
-    {
-      id: 3,
-      nome: "Professor 3",
-      foto: "https://github.com/luistomasini.png",
-      descricao: "Descricao do professor 3",
-      valor_hora: 300
-    },
-    {
-      id: 4,
-      nome: "Professor 4",
-      foto: "https://github.com/luistomasini.png",
-      descricao: "Descricao do professor 4",
-      valor_hora: 400
-    },
-  ]
+  const { 
+    listaProfessores, 
+    nome, 
+    setNome, 
+    email, 
+    setEmail, 
+    professorSelecionado, setProfessorSelecionado 
+  }  = useIndex();
   
   return (
-   <Box sx={{ backgroundColor: 'secondary.main' }}>
-      <Lista professores={professores}></Lista>
-   </Box>
+   <div>
+      <Box sx={{ backgroundColor: 'secondary.main' }}>
+        <Lista 
+          professores={listaProfessores}
+          onSelect={(professor) => setProfessorSelecionado(professor)}
+        ></Lista>
+      </Box>
+      <Dialog open={true} fullWidth PaperProps={{sx: {p: 5}}}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField 
+              label="Digite o nome"
+              type="text"
+              fullWidth
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField 
+              label="Digite o email"
+              type="email"
+              fullWidth
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+
+            />
+          </Grid>
+        </Grid>
+
+        <DialogActions sx={{mt: 5}}>
+          <Button>Cancelar</Button>
+          <Button>Marcar</Button>
+        </DialogActions>
+      </Dialog>
+   </div>
   )
 }
 
---
+++ MyTeacher/frontend/src/hooks/pages/useIndex.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { Professor } from "../../@types/professor";
+import { ApiService } from "../../services/ApiService";
+
+export function useIndex(){
+    const [listaProfessores, setListaProfessores] = useState<Professor[]>([]);
+    const[nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [professorSelecionado, setProfessorSelecionado] = useState<Professor | null>(null)
+
+    useEffect(() => {
+        ApiService.get('/professores').then((resposta) => {
+            setListaProfessores(resposta.data)
+        })
+    }, [])
+
+      return{
+        listaProfessores,
+        nome,
+        setNome,
+        email,
+        setEmail,
+        professorSelecionado,
+        setProfessorSelecionado
+      }
+}
